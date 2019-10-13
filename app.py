a3aa6566fe72f8fe87b343d88565488f030bc827
simon sayz <smkabz@gmail.com>
2019-10-13 16:21:22 +0300

    tensorflow env fix

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from contextlib import contextmanager
 from sqlalchemy import create_engine
 import json
 import sys
+# Just disables the warning, doesn't enable AVX/FMA
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 ## More Imports
 import numpy as np ## Numpy
